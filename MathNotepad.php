847c053f54ae118e6667bdf510eb5656a9354568
Pratik Karmakar <pratik.karmakar@iic.ac.in>
2018-12-15 13:09:59 +0530

    Test Commit2

--- MathNotepad.php
+++ MathNotepad.php
@@ -5,7 +5,7 @@ namespace karmakarpatrick\mathnotepad;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class MathNotepad extends \yii\base\Widget
 {
     public function run()
     {
