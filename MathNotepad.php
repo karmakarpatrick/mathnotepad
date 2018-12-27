4929e66096f4da3dde8cdc05c2107561eb94386e
Pratik Karmakar <pratik.karmakar@iic.ac.in>
2018-12-27 17:28:39 +0530

    bug Fix

--- MathNotepad.php
+++ MathNotepad.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace karmakarpatrick\mathnotepad;
-
+use yii\base\Widget;
 /**
  * This is just an example.
  */
-class MathNotepad extends \yii\base\Widget
+class MathNotepad extends Widget
 {
     public function init()
     {
         if ($this->jsFile === null) {
             $file = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tabview.css';
 
-            $this->cssFile = Yii::app()->getAssetManager()->publish($file);
+            $this->cssFile = \Yii::app()->getAssetManager()->publish($file);
         }
         parent::init();
     }
